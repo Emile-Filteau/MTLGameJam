72313ec6f3afb609d13f41c92919e212e54f063a
Émile Filteau-Tessier <emile.filteau@gmail.com>
2014-01-24 23:34:35 -0500

    scene1
    Signed-off-by: Émile Filteau-Tessier <emile.filteau@gmail.com>

--- js/game/Area.js
+++ js/game/Area.js
@@ -19,11 +19,11 @@ var Area = Base.extend({
     },
 
     draw : function(canvas, context, camera) {
-        console.log(this.backgound.width, this.backgound.height)
+        console.log(camera.width, camera.height - 100)
         context.drawImage(this.backgound, -camera.halfWidth, -camera.halfHeight);
 
-        context.drawImage(this.foreground, 0, 0);
+        context.drawImage(this.foreground, 0, 140, 857, 200);
 
-        //context.drawImage(this.ground, 0, camera.height - 195, 1920, 195);
-    }
+        context.drawImage(this.ground, 0, 300);
+}
 });

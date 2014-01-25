b58fe985e2f8e6aaa1b59bb2510560a6003b4aaf
Émile Filteau-Tessier <emile.filteau@gmail.com>
2014-01-24 22:56:09 -0500

    stuff
    Signed-off-by: Émile Filteau-Tessier <emile.filteau@gmail.com>

--- js/game/ExampleGame.js
+++ js/game/ExampleGame.js
@@ -1,42 +1,43 @@
+include("js/game/Area.js");
+
 var ExampleGame = Game.extend({
 	constructor : function(cameraWidth, cameraHeight) {
 		this.base(cameraWidth, cameraHeight);
-		
-		this.squares = [];
-		this.randyCounter = 0;
-		this.randyThreshold = 2000;
 
+        this.areas = [];
+        this.currentArea = "";
+
+
+        this.addArea("main", new Area(5000, 2000, "/images/Background.png", "/images/Middle_ground.png", "/images/Ground1.png"));
+        this.changeArea("main");
+        //this.player = new Player();
 	},
+
+    addArea : function(areaName, area) {
+        this.areas[areaName] = area;
+    },
+
+    changeArea : function(areaName) {
+        this.currentArea = areaName;
+    },
 	
 	draw : function(canvas, context) {
 		context.fillStyle = "#666";
 		context.fillRect(0, 0, canvas.width, canvas.height);
-		
-		
-		for(i in this.squares) {
-			square = this.squares[i];
-			context.save();
-			context.fillStyle = '#FFFF00';
-			context.fillRect(square.x, square.y, 10, 10);
-			context.restore();
-		}
+
+        this.areas[this.currentArea].draw(canvas, context);
 	},
 
 	update : function(framerate) {
-		this.randyCounter += framerate;
-		if(this.squares.length < 20) {
-			if(this.randyCounter >= this.randyThreshold) {
-				var rand = Math.random();
-				if(rand > 0.5) {
-					this.squares.push({x:Math.random()*400, y:Math.random()*400});		
-				}
-				this.randyCounter=0;
-			}
-		}
+
 	},
-	
-	clearSquares : function() {
-		this.squares = [];
-	}
-	
+
+    keypress : function(key) {
+
+    },
+
+
+    keyrelease : function(key) {
+
+    }
 });
